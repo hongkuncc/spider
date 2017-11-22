226b9a520968bf6bb373ba4c653ca331a8d930c7
chk <hongkuncc@163.com>
2017-11-22 16:42:36 +0800

    非常简单的词云制作

---
+++ simplewordcloud.py
@@ -0,0 +1,14 @@
+﻿# encoding:utf-8
+import matplotlib.pyplot as plt
+from wordcloud import  WordCloud
+import jieba
+
+text_path=open(r'2.txt').read()
+
+word_list=jieba.cut(text_path,cut_all=True)
+word_split=" ".join(word_list)
+my_wordcloud=WordCloud(font_path="simsun.ttf").generate(word_split)
+
+plt.imshow(my_wordcloud)
+plt.axis("off")
+plt.show()
